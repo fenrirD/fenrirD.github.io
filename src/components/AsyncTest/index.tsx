a7d7ea5ddbe2b6849d28e652be3d5584010bbffd
fenrirD <fenrir4504@gmail.com>
2023-10-20 11:34:10 +0900

    경력 날짜 오타 수정

--- src/components/AsyncTest/index.tsx
+++ src/components/AsyncTest/index.tsx
@@ -1,46 +1,45 @@
 import React, {useState} from 'react';
-import {toast} from "react-toastify";
 
 
 const AsyncTest = () => {
 
-  const [foods, setFoods] = useState([]);
+    const [foods, setFoods] = useState([]);
 
-  function order() {
-    const foods = ['피자', '치킨', '물'];
-    foods.forEach(cook);
-  }
+    function order() {
+        const foods = ['피자', '치킨', '물'];
+        foods.forEach(cook);
+    }
 
-  function cook(food) {
-    const ms = Math.floor(Math.random() * 10000);
-    console.log('조리 시간:', ms);
-    setTimeout(()=> {
-      serve(food, ms)
-    },ms)
-  }
+    function cook(food) {
+        const ms = Math.floor(Math.random() * 10000);
+        console.log('조리 시간:', ms);
+        setTimeout(() => {
+            serve(food, ms)
+        }, ms)
+    }
 
-  function serve(food, time) {
-    time = time / 1000;
-    setFoods((old) => [...old, {food, time}]);
-    console.log("서빙 : ", food);
-    const a = new Promise(()=>{
+    function serve(food, time) {
+        time = time / 1000;
+        setFoods((old) => [...old, {food, time}]);
+        console.log("서빙 : ", food);
+        const a = new Promise(() => {
 
-    })
-  }
+        })
+    }
 
-  return (
-    <div style={{display: 'flex', flex: '1', flexDirection: 'column'}}>
-      <div onClick={order} style={{display: 'flex', flexDirection: 'column'}}>
-        <button> 주문</button>
-      </div>
-      <hr/>
-      <div>
-        {
-          foods.map(({food, time}, idx) => <div key={"food_" + idx}>음식:{food}, 시간: {time} 초</div>)
-        }
-      </div>
-    </div>
-  )
+    return (
+        <div style={{display: 'flex', flex: '1', flexDirection: 'column'}}>
+            <div onClick={order} style={{display: 'flex', flexDirection: 'column'}}>
+                <button> 주문</button>
+            </div>
+            <hr/>
+            <div>
+                {
+                    foods.map(({food, time}, idx) => <div key={"food_" + idx}>음식:{food}, 시간: {time} 초</div>)
+                }
+            </div>
+        </div>
+    )
 }
 
 export default AsyncTest
